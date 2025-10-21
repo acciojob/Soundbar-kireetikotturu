d1c4bd076e42a57316a26d81a54757e0becbb8f3
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-10-21 13:17:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //your JS code here. If required.
 let musicButtons = document.querySelectorAll(".btn")
-let audioEle = document.getElementById("audio-element")
 let audio = null;
 
 document.querySelector(".stop").addEventListener("click", ()=>{
       if(audio){
 		audio.pause();
+		audio.currentTime = 0;
+		audio = null;
 	  }
-	  audioEle.pause();
     })
 
 musicButtons.forEach((item)=>{
   item.addEventListener("click", ()=>{
 	if(audio){
 		audio.pause();
+		audio.currentTime=0;
 	  }
-    let audioFile = item.textContent;
+    let audioFile = item.textContent.toLowerCase();
     audio = new Audio(`sounds/${audioFile}.mp3`)
-	audioEle.src=`sounds/${audioFile}.mp3`;
     audio.play();
   })
 })
