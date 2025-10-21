748f583fbe867f806a9abc57520d414d6a546f3f
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-10-21 13:41:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ let musicButtons = document.querySelectorAll(".btn")
 let audio = null;
 
 document.querySelector(".stop").addEventListener("click", ()=>{
-      if(audio){
-		audio.pause();
-		audio.currentTime = 0;
-		audio = null;
+    if(audio){
+      audio.pause();
+      audio.currentTime = 0;
 	  }
     })
 
 musicButtons.forEach((item)=>{
-  item.addEventListener("click", ()=>{
-	if(audio){
-		audio.pause();
-		audio.currentTime=0;
-	  }
-    let audioFile = item.textContent.toLowerCase();
-    audio = new Audio(`sounds/${audioFile}.mp3`)
+    item.addEventListener("click", ()=>{
+    if(audio){
+      audio.pause();
+      audio.remove();
+    }
+    const audioFile = item.dataset.sound;
+    audio = document.createElement("audio");
+    audio.src = `sounds/${audioFile}.mp3`;
+    document.body.appendChild(audio);
     audio.play();
   })
 })
