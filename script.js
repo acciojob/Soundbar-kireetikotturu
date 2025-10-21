2bd87a52aa10b8da6bccfa561e3d3ec90c9f2d7e
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-10-21 12:56:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ musicButtons.forEach((item)=>{
   item.addEventListener("click", ()=>{
     let audioFile = item.textContent;
     let audio = new Audio(`sounds/${audioFile}.mp3`)
+	audioEle.src=`sounds/${audioFile}.mp3`;
     audio.play();
 
     document.querySelector(".stop").addEventListener("click", ()=>{
       audio.pause();
+	  audioEle.pause();
     })
   })
 })
